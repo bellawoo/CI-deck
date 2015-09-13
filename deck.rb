f68f4bfa1c39e49ac0f814b8ba2e5e94ccbe03bd
Bella <woo.bella@gmail.com>
2015-09-13 16:56:48 -0400

    added deck methods applicable to all games

--- deck.rb
+++ deck.rb
@@ -1,7 +1,28 @@
 class Deck
+  attr_reader :cards
+
   def initialize
-    @rank = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
-    @suit = [:C, :H, :D, :S]
-    @cards = @rank.product(@suit).shuffle!
+    @rank = [*2..10, 'Jack', 'Queen', 'King', 'Ace']
+    @suit = %w[Clubs Diamonds Hearts Spades]
+    @cards = @rank.product(@suit)
+    @cards_drawn = []
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def draw
+    c = @cards.shift
+    @cards_drawn.push(c)
+    return c
+  end
+
+  def drawn
+    @cards_drawn
+  end
+
+  def cut
+    @cards.rotate!(@cards.count / 2)
   end
 end
